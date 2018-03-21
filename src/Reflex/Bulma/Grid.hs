65a77716a0295a36afd8a4bfbb4f43d183d193a2
Adrian Parvin D. Ouano <adrianparvino@gmail.com>
2018-03-21 17:35:51 +0800

    Made `columns` more general

--- src/Reflex/Bulma/Grid.hs
+++ src/Reflex/Bulma/Grid.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs, RankNTypes, DataKinds, OverloadedStrings #-}
+{-# LANGUAGE GADTs, RankNTypes, DataKinds, OverloadedStrings, ScopedTypeVariables #-}
 module Reflex.Bulma.Grid
   ( columns
   ) where
@@ -12,8 +12,8 @@ import Control.Monad.Fix
 import Data.Text (Text)
 import qualified Data.Text as T
 
-columns :: MonadWidget t m => [Text] -> (forall n. MonadFix n => (forall b. [Text] -> m b -> n b) -> n a) -> m a
+columns :: forall t m a. MonadWidget t m => [Text] -> ((forall b. [Text] -> m b -> m b) -> m a) -> m a
 columns classes m = divClass (T.unwords $ "columns":classes) $ m column
   where
-    column :: MonadWidget t m => [Text] -> m b -> m b
+    column :: [Text] -> m b -> m b
     column classes inner = divClass (T.unwords $ "column":classes) inner
